d2caeeb997e32f2a9467d0fda5d7e47271f69bf2
abbas20e2062 <javankamran@yahoo.com>
2025-06-14 12:46:40 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = "abbas-ai-cache-v1";
+const CACHE_NAME = "abbas-ai-cache-v2";
 const urlsToCache = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", event => {
